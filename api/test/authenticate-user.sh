cfd00ef0d0ce77ddd2cc81685ac3c33270e849b3
Jonas Veron <jonas.veron@hotmail.com>
2025-01-03 18:10:54 +0100

    specify Node.js version

--- api/test/authenticate-user.sh
+++ api/test/authenticate-user.sh
@@ -1,1 +1,1 @@
-curl -H 'Content-Type: application/json' -d '{"email": "laura@gimenez.com", "password":"123123123"}' http://localhost:3000/users/auth -v
+curl -H 'Content-Type: application/json' -d '{"email": "jonas@veron.com", "password":"123123123"}' https://dancetapp-final-project.onrender.com//users/auth -v
